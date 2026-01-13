ed209de6630c96344a1bd562a75862a8042c204b
BHCGH92 <ben.holt1992@live.co.uk>
2026-01-13 19:31:32 +0000

    implemented tool and category models along with a custom admin interface

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Tool
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+@admin.register(Tool)
+class ToolAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'owner',
+        'price_per_day',
+        'is_available',
+        'created_at',
+    )
+    # Adds a sidebar filter to help navigate larger data sets
+    list_filter = ('category', 'is_available')
+    # Adds a search bar for better User experience
+    search_fields = ('name', 'description')
--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Model representing tool categories (For Example Gardening, Power Tools etc.).
+    Will help organize the library for better UX.
+    """
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Tool(models.Model):
+    """
+    Model representing an individual tool listed by a user.
+    Demonstrates CRUD and data modelling techniques.
+    """
+    category = models.ForeignKey(
+        'Category', 
+        null=True, 
+        blank=True, 
+        on_delete=models.SET_NULL
+    )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='tools'
+    )
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price_per_day = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
