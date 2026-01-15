5261918520a5dd3ec99659ee6d4b607dc1ccfb23
BHCGH92 <ben.holt1992@live.co.uk>
2026-01-15 21:23:31 +0000

    checkout app model creaetd, admin view setup and initial test written.

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+class TestOrderModel(TestCase):
+    def test_order_string_method(self):
+        """Tests that the order number is used as the string representation"""
+        order = Order.objects.create(
+            full_name="Test User", 
+            email="test@test.com", 
+            order_number="12345",
+            order_total=15.00
+        )
+        self.assertEqual(str(order), "12345")
