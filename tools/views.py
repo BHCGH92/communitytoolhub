7944326ac1590c44088304616295dbf61059cc06
BHCGH92 <ben.holt1992@live.co.uk>
2026-01-16 18:16:34 +0000

    created a view in tools to display data, added two lines in settings.py and urlpatterns line in urls.py in config to fix broken image on tools template page

--- tools/views.py
+++ tools/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Tool
 
-# Create your views here.
+def all_tools(request):
+    """ 
+    A view to show all tools, including sorting and search queries.
+    """
+    # 1. Fetch all tool objects from the database
+    tools = Tool.objects.all()
+
+    # 2. Define the context (the data being sent to the template)
+    context = {
+        'tools': tools,
+    }
+
+    # 3. Return the render function with the template path
+    return render(request, 'tools/tools.html', context)
